4e0630357534b1b6fc2ad9adf3ec0fdc88b80170
Ervin Picardal <ervinjohnpicardal@gmail.com>
2023-10-27 07:56:48 +0800

    fixed eyes

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-import cv2
-import numpy as np 
-import time
+import tkinter as tk
+from PIL import Image, ImageTk
+from itertools import count
 
+class ImageLabel(tk.Label):
+    """a label that displays images, and plays them if they are gifs"""
+    def load(self, im):
+        if isinstance(im, str):
+            im = Image.open(im)
+        self.loc = 0
+        self.frames = []
 
+        try:
+            for i in count(1):
+                self.frames.append(ImageTk.PhotoImage(im.copy()))
+                im.seek(i)
+        except EOFError:
+            pass
 
+        try:
+            self.delay = im.info['duration']
+        except:
+            self.delay = 100
 
-while True:
-    #This is to check whether to break the first loop
-    isclosed=0
-    cap = cv2.VideoCapture('robot_blink.mp4')
-    while (True):
-
-        ret, frame = cap.read()
-        # It should only show the frame when the ret is true
-        if ret == True:
-
-            cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-            cv2.setWindowProperty("frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) == 27:
-                # When esc is pressed isclosed is 1
-                isclosed=1
-                break
+        if len(self.frames) == 1:
+            self.config(image=self.frames[0])
         else:
-            break
-    # To break the loop if it is closed manually
-    if isclosed:
-        break
-    time.sleep(5)
+            self.next_frame()
 
+    def unload(self):
+        self.config(image="")
+        self.frames = None
 
+    def next_frame(self):
+        if self.frames:
+            self.loc += 1
+            self.loc %= len(self.frames)
+            self.config(image=self.frames[self.loc])
+            self.after(self.delay, self.next_frame)
 
-cap.release()
-cv2.destroyAllWindows()
+root = tk.Tk()
+root.attributes('-fullscreen',True)
+
+lbl = ImageLabel(root, width=1920,height=1080)
+lbl.configure(background='black')
+lbl.pack()
+lbl.load('robot_eyes.gif')
+root.mainloop()
